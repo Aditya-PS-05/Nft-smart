d1b9ae60dc4886ebcff5cb4aa33e6731fc0594fa
Aditya-PS-05 <adityapratapsjnhh7654@gmail.com>
2024-10-22 23:41:15 +0530

    added basic deployment script

--- src/BasicNft.sol
+++ src/BasicNft.sol
@@ -6,8 +6,20 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 contract BasicNft is ERC721{
 
     uint256 private s_tokenCounter;
+    mapping(uint256 => string) private s_tokenIdToUri;
 
-    constructor() ERC721("Basic NFT", "BNFT") {
+    constructor() ERC721("Basic_NFT", "BNFT") {
         s_tokenCounter = 0;
+    }
+
+    function mintToken(string memory newTokenUri) public {
+        s_tokenIdToUri[s_tokenCounter] = newTokenUri;
+        _safeMint(msg.sender, s_tokenCounter);
+
+        s_tokenCounter++;
+    }
+
+    function tokenUri(uint256 tokenId) public view returns (string memory) {
+        return s_tokenIdToUri[tokenId];
     }
 }
